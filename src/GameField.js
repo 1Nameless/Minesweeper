3001908439402d278506f77be2232acce81210c2
Matthias <mattias.reschke@student.fh-kiel.de>
2022-10-26 22:58:28 +0200

    initial commit basic setup done to start with the actual game

---
+++ src/GameField.js
@@ -0,0 +1,65 @@
+import React, {Component} from "react";
+import open1 from "./Resources/open1.gif";
+import blank from "./Resources/blank.gif";
+import { Minesweeper } from "./Minesweeper"
+
+
+
+
+export class GameField extends Component{
+
+    constructor() {
+        super();
+
+        this.state={
+            field: new Array(10)
+        }
+
+    }
+
+    setField(newField){
+        this.setState({field:newField});
+    }
+
+    game = new Minesweeper(10, 10, 5, this.setField.bind(this))
+
+
+    width = 20;
+    height = 20;
+    bombCount = 10;
+
+
+    render(){
+        return (
+            <table className={"grid"}>
+                <tbody>
+                {
+                    this.game.grid.map((row, cr) => <tr>{
+                        row.map(
+                            (col, cc) => <td>{
+                                this.getTile(col, cr, cc)
+                            }</td>
+                        )
+                    }</tr>)
+                }
+                </tbody>
+            </table>
+
+        )
+    }
+
+
+    getTile(val, x, y) {
+
+        if (val.type === 0) {
+            return <img className={"tile"} src={blank} height={30} width={30} alt={""} onClick={() => {
+                this.game.switch(x, y)
+            }}/>
+        } else return <img className={"tile"} src={open1} height={30} width={30} alt={""} onClick={() => {
+            this.game.switch(x, y)
+        }} />
+    }
+
+
+
+}
---
+++ src/Minesweeper.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import {GameField} from "./GameField";
+
+
+
+class Tile{
+    isCovered
+    type
+    flaged
+    constructor(isCovered, type) {
+        this.isCovered = isCovered;
+        this.type = type;
+        this.flaged = false;
+    }
+}
+
+
+export class Minesweeper{
+
+    width
+    height
+    bombs
+    updateMethod
+    grid
+
+    constructor(height, width, bombs, updateMethode) {
+        this.bombs = bombs;
+        this.height = height;
+        this.width = width;
+        this.updateMethod = updateMethode;
+
+        this.grid = new Array(this.height)
+
+        for (let i = 0; i < this.height; i++) {
+            (this.grid)[i] = new Array(this.width);
+        }
+
+        //fill field initially with all 0
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                this.grid[i][j] = new Tile(true, 0);
+            }
+        }
+
+    }
+
+
+    switch(x, y){
+
+        if(this.grid[x][y].type === 0) this.grid[x][y].type = 1;
+        else this.grid[x][y].type = 0;
+        this.updateMethod(this.grid);
+
+    }
+
+
+
+
+}
